7cf6913633830fbbd80b17da0442e6c995c3b4ea
shiveshchauhan <115156782+shiveshchauhan@users.noreply.github.com>
2023-11-01 15:01:43 +0530

    Add files via upload

---
+++ db_027_nata_p4.sql
@@ -0,0 +1,276 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 20, 2023 at 04:41 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_027_nata_p4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history_lelang`
+--
+
+CREATE TABLE `history_lelang` (
+  `id_history` int(11) NOT NULL,
+  `id_lelang` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `penawaran_harga` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_barang`
+--
+
+CREATE TABLE `tb_barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `tgl` date NOT NULL,
+  `harga_awal` int(20) NOT NULL,
+  `deskripsi_barang` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+INSERT INTO `tb_barang` (`id_barang`, `nama_barang`, `tgl`, `harga_awal`, `deskripsi_barang`) VALUES
+(1, 'Ford Mustang', '2023-04-29', 400000, 'A charismatic sports car that gives you lots for your money'),
+(2, 'BMW i4', '2023-04-28', 600000, 'Capable electric coupe with lots of practicality'),
+(3, 'Lamborghini Aventador', '2023-04-27', 200000, 'A stunning supercar that sounds incredible'),
+(4, 'Lamborghini Urus', '2023-04-24', 290000, 'An Italian supercar in large SUV form'),
+(6, 'BMW 2 Series Active Tourer', '2023-05-20', 320000, 'A practical family MPV with a premium feel'),
+(7, 'Aston Martin DB11', '2023-05-27', 750000, 'Speed and luxury in excess wrapped in a head-turning body'),
+(8, 'Ford Mustang', '2023-05-18', 480000, 'A charismatic sports car that gives you lots for your money'),
+(9, 'Honda NSX', '2023-05-17', 800000, 'A very fast hybrid supercar'),
+(10, 'Lamborghini Aventador', '2023-05-15', 900000, 'A stunning supercar that sounds incredible'),
+(11, 'Mazda MX-5 RF', '2023-05-09', 240000, 'Nimble two-seat open-top with a folding metal roof'),
+(12, 'Nissan GT-R', '2023-05-13', 900000, 'Brutally quick coupe can cope with everyday use'),
+(13, 'Porsche 911', '2023-05-12', 790000, 'A brilliant sports car that easily slots into your daily routine');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_lelang`
+--
+
+CREATE TABLE `tb_lelang` (
+  `id_lelang` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `tgl_dibuka` datetime NOT NULL DEFAULT current_timestamp(),
+  `tgl_ditutup` datetime NOT NULL DEFAULT current_timestamp(),
+  `harga_akhir` int(20) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `id_petugas` int(11) NOT NULL,
+  `status` enum('dibuka','ditutup') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_lelang`
+--
+
+INSERT INTO `tb_lelang` (`id_lelang`, `id_barang`, `tgl_dibuka`, `tgl_ditutup`, `harga_akhir`, `id_user`, `id_petugas`, `status`) VALUES
+(1, 1, '2023-04-29 14:07:00', '2023-05-06 14:07:00', 0, NULL, 1, 'dibuka'),
+(2, 2, '2023-04-28 14:08:00', '2023-05-05 14:08:00', 0, NULL, 1, 'dibuka'),
+(3, 3, '2023-04-27 14:08:00', '2023-05-03 14:08:00', 0, NULL, 1, 'dibuka'),
+(4, 4, '2023-04-24 14:11:00', '2023-04-30 14:11:00', 0, NULL, 1, 'dibuka'),
+(6, 6, '2023-05-20 09:13:00', '2023-05-27 09:13:00', 0, NULL, 1, 'dibuka'),
+(7, 7, '2023-05-27 09:58:00', '2023-05-27 09:58:00', 0, NULL, 1, 'dibuka'),
+(8, 8, '2023-05-18 10:01:00', '2023-05-25 10:01:00', 0, NULL, 1, 'dibuka'),
+(9, 9, '2023-05-17 10:02:00', '2023-05-23 10:02:00', 0, NULL, 1, 'dibuka'),
+(10, 10, '2023-05-15 10:03:00', '2023-05-27 10:03:00', 0, NULL, 1, 'dibuka'),
+(11, 11, '2023-05-09 10:04:00', '2023-05-24 10:04:00', 0, NULL, 1, 'dibuka'),
+(12, 12, '2023-05-13 10:06:00', '2023-05-27 10:06:00', 0, NULL, 1, 'dibuka'),
+(13, 13, '2023-05-12 10:06:00', '2023-05-23 10:06:00', 0, NULL, 1, 'dibuka');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_level`
+--
+
+CREATE TABLE `tb_level` (
+  `id_level` int(11) NOT NULL,
+  `level` enum('administrator','petugas') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_level`
+--
+
+INSERT INTO `tb_level` (`id_level`, `level`) VALUES
+(1, 'administrator'),
+(2, 'petugas');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_masyarakat`
+--
+
+CREATE TABLE `tb_masyarakat` (
+  `id_user` int(11) NOT NULL,
+  `nama_lengkap` varchar(25) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `telp` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_petugas`
+--
+
+CREATE TABLE `tb_petugas` (
+  `id_petugas` int(11) NOT NULL,
+  `nama_petugas` varchar(25) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `id_level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_petugas`
+--
+
+INSERT INTO `tb_petugas` (`id_petugas`, `nama_petugas`, `username`, `password`, `id_level`) VALUES
+(1, 'admin', 'natainditama', '$2y$10$xkHWEWW.c4qFk9hH1p/3aO5sbSkPCO9tRQ8Bw9/IVWXBkEWZBPv/C', 2);
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `history_lelang`
+--
+ALTER TABLE `history_lelang`
+  ADD PRIMARY KEY (`id_history`),
+  ADD KEY `id_barang` (`id_barang`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_lelang` (`id_lelang`);
+
+--
+-- Indexes for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `tb_lelang`
+--
+ALTER TABLE `tb_lelang`
+  ADD PRIMARY KEY (`id_lelang`),
+  ADD KEY `id_barang` (`id_barang`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_petugas` (`id_petugas`);
+
+--
+-- Indexes for table `tb_level`
+--
+ALTER TABLE `tb_level`
+  ADD PRIMARY KEY (`id_level`);
+
+--
+-- Indexes for table `tb_masyarakat`
+--
+ALTER TABLE `tb_masyarakat`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `tb_petugas`
+--
+ALTER TABLE `tb_petugas`
+  ADD PRIMARY KEY (`id_petugas`),
+  ADD UNIQUE KEY `username` (`username`) USING BTREE,
+  ADD KEY `id_level` (`id_level`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `history_lelang`
+--
+ALTER TABLE `history_lelang`
+  MODIFY `id_history` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `tb_lelang`
+--
+ALTER TABLE `tb_lelang`
+  MODIFY `id_lelang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `tb_level`
+--
+ALTER TABLE `tb_level`
+  MODIFY `id_level` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tb_masyarakat`
+--
+ALTER TABLE `tb_masyarakat`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tb_petugas`
+--
+ALTER TABLE `tb_petugas`
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `history_lelang`
+--
+ALTER TABLE `history_lelang`
+  ADD CONSTRAINT `history_lelang_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `tb_barang` (`id_barang`) ON DELETE CASCADE,
+  ADD CONSTRAINT `history_lelang_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tb_masyarakat` (`id_user`) ON DELETE CASCADE,
+  ADD CONSTRAINT `history_lelang_ibfk_3` FOREIGN KEY (`id_lelang`) REFERENCES `tb_lelang` (`id_lelang`);
+
+--
+-- Constraints for table `tb_lelang`
+--
+ALTER TABLE `tb_lelang`
+  ADD CONSTRAINT `tb_lelang_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `tb_barang` (`id_barang`) ON DELETE CASCADE,
+  ADD CONSTRAINT `tb_lelang_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tb_masyarakat` (`id_user`),
+  ADD CONSTRAINT `tb_lelang_ibfk_3` FOREIGN KEY (`id_petugas`) REFERENCES `tb_petugas` (`id_petugas`);
+
+--
+-- Constraints for table `tb_petugas`
+--
+ALTER TABLE `tb_petugas`
+  ADD CONSTRAINT `tb_petugas_ibfk_1` FOREIGN KEY (`id_level`) REFERENCES `tb_level` (`id_level`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
